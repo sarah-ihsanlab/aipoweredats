0476435b895d36447a20060fdbc9e0ea5b90a857
sarah-ihsanlab <sarah.ihsanlab@gmail.com>
2026-01-04 17:08:27 +0400

    First commit, adding all files

---
+++ app/routes/auth.tsx
@@ -0,0 +1,14 @@
+
+export const meta = () => ([
+    { title: "ResumeUp | Auth" },
+    { name: 'description', content: 'Log into your account' },
+
+])
+
+const auth = () => {
+    return (
+        <div>auth</div>
+    )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "./../../constants";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeUp" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track your applications & resume ratings</h1>
+      </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+  </main>;
 }
